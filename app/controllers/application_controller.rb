d8e9ea8f5ba49e07bca323d744c4ba568dd67b09
sara lai <shihualai@gmail.com>
2025-11-17 20:41:10 +0800

    first attempts at landing page & signup/signin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    puts "this is working - custom redirect after sign in"
+    dashboard_path
+  end
+
+
+  def after_sign_up_path_for(resource)
+     puts "this is working - custom redirect after sign up"
+    dashboard_path
+  end
 end
