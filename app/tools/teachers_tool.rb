881d0cf534a36f60fa4008fd19fe90bba92f80e4
sara lai <shihualai@gmail.com>
2025-11-21 12:42:44 +0800

    tool experiment breaking heroku

--- app/tools/teachers_tool.rb
+++ app/tools/teachers_tool.rb
@@ -1,18 +1,18 @@
-require 'open-uri'
+# require 'open-uri'
 
-class AvailableTeachersTool < RubyLLM::Tool
-  description "Gets available teachers for a batch."
+# class AvailableTeachersTool < RubyLLM::Tool
+#   description "Gets available teachers for a batch."
 
-  def initialize(batch_number:)
-    @batch_number = batch_number
-  end
+#   def initialize(batch_number:)
+#     @batch_number = batch_number
+#   end
 
-  def execute
-    url = "https://kitt.lewagon.com/api/v1/camps/#{@batch_number}/todays_teachers"
+#   def execute
+#     url = "https://kitt.lewagon.com/api/v1/camps/#{@batch_number}/todays_teachers"
 
-    response = URI.parse(url).read
-    JSON.parse(response)
-  rescue => e
-    { error: e.message }
-  end
-end
+#     response = URI.parse(url).read
+#     JSON.parse(response)
+#   rescue => e
+#     { error: e.message }
+#   end
+# end
